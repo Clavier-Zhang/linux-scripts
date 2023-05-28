b3b9cef2585dc384e55127ec39cb98467d46ce16
Nakiri Clavier <yichi.private@outlook.com>
2023-05-27 20:47:51 -0700

    init

---
+++ install-miniconda.sh
@@ -0,0 +1,5 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-aarch64.sh
+bash Miniconda3-py310_23.3.1-0-Linux-aarch64.sh
+rm Miniconda3-py310_23.3.1-0-Linux-aarch64.sh
+
+
