ec9421de145abeabf5797e658b4e3018d1a1dafb
Nakiri Clavier <yichi.private@outlook.com>
2023-05-27 21:32:21 -0700

    Add script for pip install

---
+++ install-mem-proc-fs.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libusb-1.0 fuse openssl lz4
+# https://github.com/ufrisk/MemProcFS/wiki/_Linux
+sudo apt-get install make gcc pkg-config libusb-1.0 libusb-1.0-0-dev fuse libfuse-dev openssl libssl-dev lz4 liblz4-dev
+pip install memprocfs
